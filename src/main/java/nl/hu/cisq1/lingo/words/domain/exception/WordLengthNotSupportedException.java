cfaef0e7e3daa8da3ee43c9b3ee72c76d41c342f
Jp <jan-paul.vandermoolen@student.hu.nl>
2021-02-08 17:08:50 +0100

    ....

--- src/main/java/nl/hu/cisq1/lingo/words/domain/exception/WordLengthNotSupportedException.java
+++ src/main/java/nl/hu/cisq1/lingo/words/domain/exception/WordLengthNotSupportedException.java
@@ -1,6 +1,11 @@
 package nl.hu.cisq1.lingo.words.domain.exception;
 
 public class WordLengthNotSupportedException extends RuntimeException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
     public WordLengthNotSupportedException(Integer length) {
         super("Could not find word of length " + length);
     }
