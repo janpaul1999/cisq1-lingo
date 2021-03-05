7d13fa2353efc14770a9e95ddf4a66b576603036
Jp <jan-paul.vandermoolen@student.hu.nl>
2021-03-05 16:38:22 +0100

    Slight changes go brrrrr

--- src/test/java/trainer/domain/FeedbackTest.java
+++ src/test/java/trainer/domain/FeedbackTest.java
@@ -10,10 +10,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class FeedbackTest {
 
     @Test
-    @DisplayName("word is guessed if all letters are correct")
+    @DisplayName("word is guessed if all letters are CORRECT")
     void wordIsGuessed(){
     Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT));
     assertTrue(feedback.isWordGuessed());
+    }
+
+    @Test
+    @DisplayName("word is not guessed if not all letters are CORRECT")
+    void wordIsNotGuessed(){
+        Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT,Mark.INVALID,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT));
+        assertFalse(feedback.isWordGuessed());
     }
 
 }
